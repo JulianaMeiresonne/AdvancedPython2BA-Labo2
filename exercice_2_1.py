930a00f16530c2a46914ddfbb07329e9c6f9f359
Juliana <23342ecam@gmail.com>
2025-03-18 10:40:36 +0100

    Exercice 2 terminer

--- exercice_2_1.py
+++ exercice_2_1.py
@@ -6,3 +6,26 @@ Line 3: 829, -12, 88
 Line 12: 99 Line 28: +15
 Astuce : Pour lire le paramètre de la ligne de commande, importez le module sys et utilisez la liste sys.args qui va contenir tous ces paramètres.
 '''
+import re
+
+def computefile(path: str):
+    pattern0 = r'[+-]?[0-9]+'#numéro de téléphone , r'^[+-]?[0-9]{0,100}$' = cela prend en compte '' car 0 c'est rien donc 0='', si je commence à 1 pas de probleme mais + = permet de faire de 1 à +inf
+    # r'^[+-]?[0-9]+$' est trop severe car ^impose que le debut match et $ impose que la fin match
+    p0 = re.compile(pattern0)
+    mots = {}
+    line = None
+    i =0
+    try:
+        with open(path) as file:
+            while line !='': # tant que line n'est pas à la dernier ligne de mon fichier '' alors ma boucle while continue
+                i += 1
+                line = file.readline() # il lit une ligne à chaque fois qu'il est applé
+                if p0.match(line) != None:
+                  mots[f'Line{i}'] = p0.findall(line)
+    except FileNotFoundError:
+        print('Le fichier est introuvable')
+    except IOError:
+        print("Erreur d'entrée/sortie")  
+    return mots
+print(computefile('number.txt'))
+#Refaire l'exercices avec la fonction sys voir photo
