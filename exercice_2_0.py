ddac64ff9d16c7ad75b406c753d437cd6f68e7e6
Juliana <23342ecam@gmail.com>
2025-03-18 09:26:21 +0100

    exercice 1 et ces test sont terminées

--- exercice_2_0.py
+++ exercice_2_0.py
@@ -12,23 +12,39 @@ import re
 pattern0 = r'^\+[0-9]{2}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}\s[0-9]{2}$'#numéro de téléphone
 p0 = re.compile(pattern0)
 
-pattern1 = r'^[\+\-]{1}[0-9]{1}$' # nombre entier
+pattern1 = r'^[\+\-]?[0-9]{1}$' # nombre entier #? permet de rendre le crochet optionel donc c'est soit +, soit - soit rien devant 
 p1 = re.compile(pattern1)
-#print(p1.match("3")) => cella doit etre mon test 
 
-pattern2 = r'^[1-9]{1}?$' #plaque d’immatriculation
+pattern2 = r'^[0-9]?[A-Z]{3}[0-9]{3}|[0-9]?[0-9]{3}[A-Z]{3}$' #plaque d’immatriculation
 p2 = re.compile(pattern2)
-print(p2.match("3"))
 
-pattern3 = r'^\+[0-9]{2}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}\s[0-9]{2}$' #nom d’un volume sous windows
+pattern3 = r'^C:\\$' #nom d’un volume sous windows
 p3 = re.compile(pattern3)
-print()
 check = "None"
-#and p1.match(Responce) and p2.match(Responce) and p3.match(Responce)
-while check == "None" :
-    print("Entrez votre numéro de téléphone sous la forme +xx xxx xx xx xx")
-    Responce = input()
-    if p0.match(Responce) is not None :
-        check = "OK"
-    else :
-        print("Le numero de téléphone entré n'est pas sous +xx xxx xx xx xx, réessayez!!")  
+
+
+if __name__ == '__main__':
+    while check == "None" :
+        print("Entrez votre numéro de téléphone sous la forme +xx xxx xx xx xx")
+        Responce0 = input()
+        print("Entrez nombre entier")
+        Responce1 = input()
+        print("Entrez votre plaque d’immatriculation sous la forme XLLLDDD ou XDDDLLL")
+        Responce2 = input()
+        print("Entrez Le nom d’un volume sous windows sous la forme C:\\")
+        Responce3 = input()
+        if p0.match(Responce0) and p1.match(Responce1) and p2.match(Responce2) and p3.match(Responce3) is not None :
+            check = "OK"
+        elif p0.match(Responce0) == "None":
+            print("Le numero de téléphone entré n'est pas correct, réessayez!!")
+        elif p1.match(Responce1) == "None":
+            print("Le nombre entier entré n'est pas correct, réessayez!!")
+        elif p2.match(Responce2) == "None":
+            print("La plaque d’immatriculation entrée n'est pas correct, réessayez!!")
+        elif p3.match(Responce3) == "None":
+            print("Le nom du volume sous windows entré n'est pas correct, réessayez!!")
+
+# Collect Error 1 : mon code de base avait une erreur, l'expressoin reeguliere était mal écrite donc on code avait une erreur quand il collecter les donner pour faire les test après
+#Collect Error 2 : mon code avait des input() donc au moment de la collect de donner ça bloquer sur le input() donc il faut mettre if __name__ == '__main__': ppur que si mon n'est pas dans le ficher main alors cette partie du code s'execute
+
+# Test project : On sera coter sur le coverage de notre project => voir S1 coverage
--- test_exercice_2_0.py
+++ test_exercice_2_0.py
@@ -5,7 +5,6 @@ import re
 #Les fonctions doivent commencer par test_ (ex: def test_addition():).
 
 def test_expression_phone_number():
-    exercice_2_0.pattern0
     p = re.compile(exercice_2_0.pattern0)
     assert p.match('+xx xxx xx xx xx') == None
     assert p.match('+33 333 33 33 33') != None
@@ -14,8 +13,23 @@ def test_expression_phone_number():
     assert p.match('33 333 33 33 33 ') == None
 
 
-def test_roots():
-    pass
+def test_expression_integers():
+    p = re.compile(exercice_2_0.pattern1)
+    assert p.match('-3') != None
+    assert p.match('0') != None
+    assert p.match('a') == None
+    assert p.match('3') != None
 
-def test_integrate():
-    pass
+def test_registration_plate():
+    p = re.compile(exercice_2_0.pattern2)
+    assert p.match('1ABC345') != None
+    assert p.match('3567TYU') != None
+    assert p.match('567TYU') != None
+    assert p.match('ABC345') != None
+    assert p.match('6784457') == None
+    assert p.match('aaaaa') == None
+    assert p.match('AAAAA') == None
+
+def test_expression_windows_volume():
+    p = re.compile(exercice_2_0.pattern3)
+    assert p.match('C:\\') != None
